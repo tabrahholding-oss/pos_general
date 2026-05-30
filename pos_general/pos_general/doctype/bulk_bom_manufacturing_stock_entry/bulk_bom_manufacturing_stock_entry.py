268955453cc373b824f509a23ac3e75b5d5b520a
Omer Younas <mianomeryounas@gmail.com>
2026-05-30 17:17:00 +0500

    Bulk BOM Manufacture

--- pos_general/pos_general/doctype/bulk_bom_manufacturing_stock_entry/bulk_bom_manufacturing_stock_entry.py
+++ pos_general/pos_general/doctype/bulk_bom_manufacturing_stock_entry/bulk_bom_manufacturing_stock_entry.py
@@ -1,9 +1,9 @@
-# Copyright (c) 2025, Hussain and contributors
+# Copyright (c) 2026, Hussain and contributors
 # For license information, please see license.txt
 
 # import frappe
 from frappe.model.document import Document
 
 
-class TabrahPosOffer(Document):
+class BulkBOMManufacturingStockEntry(Document):
 	pass
--- pos_general/pos_general/doctype/tabrah_pos_offer/test_tabrah_pos_offer.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Hussain and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestTabrahPosOffer(FrappeTestCase):
-	pass
